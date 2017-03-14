4dd8e805e612d6c13fcc15dc50b6bfcc594f063d
Joel Rorseth <joelrorseth@gmail.com>
2017-03-14 17:36:03 -0400

    Add working json file and prep work

--- app/src/main/java/com/joelrorseth/ironinspiredfitness/ExercisesActivity.java
+++ app/src/main/java/com/joelrorseth/ironinspiredfitness/ExercisesActivity.java
@@ -2,12 +2,19 @@ package com.joelrorseth.ironinspiredfitness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ExercisesActivity extends AppCompatActivity {
 
+    private ListView exercisesListView;
+
     @Override
+    // ==============================================
+    // ==============================================
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
+
+        exercisesListView = (ListView) findViewById(R.id.exercises_list_view);
     }
 }
--- app/src/main/java/com/joelrorseth/ironinspiredfitness/MyWorkoutsActivity.java
+++ app/src/main/java/com/joelrorseth/ironinspiredfitness/MyWorkoutsActivity.java
@@ -2,12 +2,17 @@ package com.joelrorseth.ironinspiredfitness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MyWorkoutsActivity extends AppCompatActivity {
+
+    private ListView workoutsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_workouts);
+
+        workoutsListView = (ListView) findViewById(R.id.workouts_list_view);
     }
 }
